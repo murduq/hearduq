64389bcf3a21964352fae86446058b2dcce3d07e
Kian Murdock <kianmurdock07@gmail.com>
2023-05-06 10:58:24 -0400

    added dupe song protection

--- duqdle/App/views.py
+++ duqdle/App/views.py
@@ -10,11 +10,16 @@ def index(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context={"page_obj":page_obj}
+    
     sotd_dict = gen_song()
-    sotd = Song.objects.create(title=sotd_dict['title'],
-                               artist=sotd_dict['artist'],
-                               image=sotd_dict['image'],
-                               audio_file=sotd_dict['audio_file'])
-    sotd.save()
+    all_songs = Song.objects.values_list('title', flat=True)
+    print(all_songs)
+    if sotd_dict['title'] not in all_songs:
+        print(f"Adding {sotd_dict['title']} - {sotd_dict['artist']}")
+        sotd = Song.objects.create(title=sotd_dict['title'],
+                                artist=sotd_dict['artist'],
+                                image=sotd_dict['image'],
+                                audio_file=sotd_dict['audio_file'])
+        sotd.save()
     
     return render(request,"index.html",context)
